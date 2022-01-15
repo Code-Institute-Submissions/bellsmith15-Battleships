ebcad87c7ae5b9417ff5aa59b81d2f878d77e9ed
bellsmith15 <smith27n@gmail.com>
2022-01-15 02:23:11 +0000

    Game over

--- run.py
+++ run.py
@@ -72,17 +72,19 @@ print_board()
 
 
 def random_row():
-    """ Prints the board """
+    """ Computer randomly picks the row of the ship """
     return randint(0, len(board)-1)
 
 
 def random_col():
-    """ Prints the board """
+    """ Computer randomly picks the column of the ship """
     return randint(0, len(board[0])-1)
 
 
 ship_row = random_row()
 ship_col = random_col()
+print(ship_row)
+print(ship_col)
 
 
 for turn in range(6):
@@ -90,9 +92,9 @@ for turn in range(6):
 
     guess_row = int(input("Guess Row: "))
     guess_col = int(input("Guess Col: "))
-    # Guess the row of where the ship is on the board
+    # Guess the row and column of where the ship is on the board
     if guess_row == ship_row and guess_col == ship_col:
-        print("HIT! You sank their battleship")
+        print("HIT! You sank the battleship")
         print("Game Over")
         break
 
